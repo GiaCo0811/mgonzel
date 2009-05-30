6d11f732bd19373126e6a4493458ec3ca5576905
mgonzel <mgonzel@0234f268-4602-11de-84a6-839b6ed6c72e>
2009-05-30 04:25:27 +0000

    se agrego la sesion y demas

--- Web-API/src/web/api/mvc/controller/AbstractControllerServlet.java
+++ Web-API/src/web/api/mvc/controller/AbstractControllerServlet.java
@@ -1,8 +1,12 @@
 package web.api.mvc.controller;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,6 +20,9 @@ public abstract class AbstractControllerServlet extends HttpServlet implements C
 
 	private static final long serialVersionUID = 7810846794288770467L;
 
+	public static String COOKIES = "cookies";
+	public static String PARAMETERS = "parameters";
+	
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -36,4 +43,41 @@ public abstract class AbstractControllerServlet extends HttpServlet implements C
 	}
 	
 	protected abstract void execute(HttpServletRequest req, HttpServletResponse res);
+	
+	/**
+	 * Carga los parametros de la pagina, incluyendo los enviados por URL.
+	 * @param req
+	 * @return
+	 */
+	protected final HashMap<String, Object> loadRequestParameters(HttpServletRequest req) {
+		HashMap<String, Object> parameters = new HashMap<String, Object>();
+		
+		Map reqParameters = req.getParameterMap();
+		Iterator paramIter = reqParameters.keySet().iterator(); 
+		while (paramIter.hasNext()){
+			String key = (String)paramIter.next();
+			
+			parameters.put(key, reqParameters.get(key));
+		}
+		
+		return parameters;
+	}
+	
+	/**
+	 * Carga las cookies de la pagina.
+	 * @param req
+	 * @return
+	 */
+	protected final HashMap<String,String> loadCookies(HttpServletRequest req){
+		HashMap<String, String> cookies = new HashMap<String, String>();
+		
+		Cookie [] arCookie = req.getCookies();
+		
+		for (Cookie cookie : arCookie){
+			cookies.put(cookie.getName(), cookie.getValue());
+		}
+		return cookies;	
+	}
+	
+
 }
--- Web-API/src/web/api/mvc/controller/FrontEndControllerServlet.java
+++ Web-API/src/web/api/mvc/controller/FrontEndControllerServlet.java
@@ -1,7 +1,11 @@
 package web.api.mvc.controller;
 
+import java.util.HashMap;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import web.api.session.SessionValidation;
 
 /**
  * Controller abstracto para las paginas estilo Front-end.
@@ -13,9 +17,50 @@ public abstract class FrontEndControllerServlet extends AbstractControllerServle
 	private static final long serialVersionUID = -8973262686871954774L;
 
 	protected void execute(HttpServletRequest req, HttpServletResponse res){
-		//contexto
-		//Cookies
-		//sesion
-		//validacion de sesion / pagina
+		
+		//Atributos del request
+		HashMap<String,Object> requestAttributes = new HashMap<String, Object>();
+		
+		HashMap<String,Object> parameters = loadRequestParameters(req);
+		requestAttributes.put("parameters", parameters);
+		
+		//Contexto
+		
+		//carga de Cookies
+		HashMap<String, String> cookies = loadCookies(req); 
+		requestAttributes.put("cookies", cookies);
+
+		//validacion de sesion
+		if (getValidated(req)){
+			boolean sessionValidated = validateSession(cookies);
+			
+			if (!sessionValidated){
+				//showLoginView(req, res);
+				return;
+			}
+		}
+		
+		executeView(req,res,requestAttributes);
+
 	}
+	
+	protected abstract void executeView(HttpServletRequest req, HttpServletResponse res,
+			HashMap<String, Object> requestAttributes);
+
+	private boolean validateSession(HashMap<String, String> cookies) {
+		String [] login = ((String)cookies.get("user")).split("-");
+		int userId = Integer.parseInt(login[0]);
+		long hash = Long.parseLong(login[1]);
+		return SessionValidation.validateSession(userId,hash);
+	}
+
+	/**
+	 * Segun el request, dice si debe validarse la sesion 
+	 * @param req
+	 * @return por default = false, o sobreescribir metodo.
+	 */
+	protected boolean getValidated(HttpServletRequest req){
+		return false;
+	}
+
 }
